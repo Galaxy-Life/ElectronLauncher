a7b4c670f307eca9c392a4ed4e82399ab3072191
Senne <senne.van.rompaey@student.howest.be>
2022-05-31 01:32:15 +0200

    Change window title & convert from tabs to spaces

--- main.js
+++ main.js
@@ -8,35 +8,35 @@ const prompt = require('electron-prompt'); //prompting system to get user URL
 
 let pluginName = null; //put the right flash plugin in depending on the operating system.
 switch (process.platform) {
-	case 'win32':
-		switch (process.arch) {
-			case 'ia32':
-			case 'x32':
-				pluginName = 'flashver/pepflashplayer32.dll'
-				console.log("ran!");
-				break
-			case 'x64':
-				pluginName = 'flashver/pepflashplayer64.dll'
-				console.log("ran!");
-				break
-		}
-		break
-	case 'linux':
-		switch (process.arch) {
-			case 'ia32':
-			case 'x32':
-				pluginName = 'flashver/libpepflashplayer.so' // added and tested :D
-				break
-			case 'x64':
-				pluginName = 'flashver/libpepflashplayer.so'
-				break
-		}
+    case 'win32':
+        switch (process.arch) {
+            case 'ia32':
+            case 'x32':
+                pluginName = 'flashver/pepflashplayer32.dll'
+                console.log("ran!");
+                break
+            case 'x64':
+                pluginName = 'flashver/pepflashplayer64.dll'
+                console.log("ran!");
+                break
+        }
+        break
+    case 'linux':
+        switch (process.arch) {
+            case 'ia32':
+            case 'x32':
+                pluginName = 'flashver/libpepflashplayer.so' // added and tested :D
+                break
+            case 'x64':
+                pluginName = 'flashver/libpepflashplayer.so'
+                break
+        }
 
-		app.commandLine.appendSwitch('no-sandbox');
-		break
-	case 'darwin':
-		pluginName = 'flashver/PepperFlashPlayer.plugin'
-		break
+        app.commandLine.appendSwitch('no-sandbox');
+        break
+    case 'darwin':
+        pluginName = 'flashver/PepperFlashPlayer.plugin'
+        break
 }
 app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, pluginName));
 //CACHE IS ENABLED but restarted on launch!
@@ -44,74 +44,34 @@ app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, pluginName
 
 let mainWindow;
 
-function promptIt(isSplash, win) { //function to handle prompting
-	prompt({
-		title: "Flash Browser",
-		label: "URL: ",
-		inputAttrs: {
-			type: 'url'
-		},
-		type: 'input'
-	})
-		.then((r) => {
-			if (r == null) {
-				console.log("user cancelled!");
-				if (!isSplash) {
-					app.quit();
-				}
-			} else {
-				console.log('result', r);
-				if (isSplash) {
-					win.loadURL(r);
-				}
-				else {
-					win.loadURL(r);
-					win.maximize();
-					setTimeout(() => {
-						win.show(); //pauses program for 1 second to allow web-page to load before rendering.
-					}, 1000);
-                }
-
-			}
-		}).catch(console.error)
-}
-
-
 app.on('ready', function () {
-	var menu = Menu.buildFromTemplate([
-		{
-			label: 'Menu',
-			submenu: [
-				{
-					label: 'Change URL',
-					click() {
-						promptIt(true, win);
-                    }
-				},
-				{
-					label: 'Exit',
-					click() {
-						app.quit();
-                    }
-				}
-			]
-		}
-	])
-	Menu.setApplicationMenu(menu);
-	
-	let win = new BrowserWindow({
-		show: false,
-		webPreferences: {
-			plugins: true
-		}
-	})
-	promptIt(false, win);
-	win.webContents.session.clearCache(function () {
-		//clearCache
-	});
-	
+
+    var menu = Menu.buildFromTemplate([]);
+    Menu.setApplicationMenu(menu);
+
+    // create window
+    let win = new BrowserWindow({
+        show: false,
+        webPreferences: {
+            plugins: true
+        },
+        title: "Galaxy Life"
+    })
+
+    // load default page
+    win.loadURL("https://game.galaxylifegame.net/game");
+    win.maximize();
+
+    //pauses program for 1 second to allow web-page to load before rendering.
+    setTimeout(() => {
+        win.show();
+    }, 1000);
+
+    win.webContents.session.clearCache(function () {
+        //clearCache
+    });
 })
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') app.quit(); //fricking darwin!! >:D
+    if (process.platform !== 'darwin') app.quit(); //fricking darwin!! >:D
 })
